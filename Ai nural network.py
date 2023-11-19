dd49d72593fafcee121df968a4647b4c06d4c718
everett2323 <cloxley4@gmail.com>
2023-11-19 11:29:24 -0500

    foward 11:29 AM

---
+++ Ai nural network.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+X = np.array(([0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]), dtype=float)
+
+y = np.array(([1], [0], [0], [0], [0], [0], [0], [1]), dtype=float)
+
+xPredict = np.array(([0,0,1]), dtype=float)
+
+
